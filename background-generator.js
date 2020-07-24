76512d8a6cb8a29337bf5276b2089809a3890a99
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-07-24 10:46:15 +0100

    code refactoring

--- background-generator.js
+++ background-generator.js
@@ -1,16 +1,12 @@
 const css = document.querySelector("h3");
-let color1 = document.getElementsByName("color1")[0];
-let color2 = document.getElementsByName("color2")[0];
+const [color1, color2] = document.querySelectorAll(".color");
 const body = document.querySelector("#gradient");
 body.style.zoom = "80%";
-const changeBackground = () => {
-    body.style.background = "linear-gradient(to right, "
-        + color1.value
-        + ", "
-        + color2.value
-        + ")";
-    css.textContent = body.style.background + ";"
 
+linearGradient = () => `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+changeBackground = () => {
+    body.style.background = linearGradient();
+    css.textContent = body.style.background + ";"
 }
 color1.addEventListener("input", changeBackground);
 color2.addEventListener("input", changeBackground);
